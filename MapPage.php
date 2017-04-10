bfef31750b6288d442b9fce1f86e3951cbf39616
Richard Huynh <Richard Huynh>
2017-04-10 00:40:27 -0700

    Added all remaining pages as blanks

---
+++ MapPage.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="generalStyle.css">
+        <script src="mapScript.js"></script>
+    </head>
+<body Style="background-image: url('pokeballBK.png'); opacity: 30%;">
+        <div style="background-color: #EF1A35; padding: 2% 0.5% 0% 0.5%;">
+            <a href = "MainPage.php">
+                <img src="pokemonLogo.png" width="20%" alt="" />
+            </a>
+            <div>
+                <table width="100%" style="border: 1px solid black; table-layout: fixed;">
+                    <tr>
+                        <th style="border: 1px solid black;">
+                            <a href="TrainerPage.php">Trainers</a>
+                        </th>
+                        <th style="border: 1px solid black;">
+                            <a href="PokemonPage.php">Pokemons</a>
+                        </th>
+                        <th style="border: 1px solid black;">
+                            <a href="HMPage.php">HMs & TMs</a>
+                        </th>
+                        <th style="border: 1px solid black;">
+                            <a href="NPCPage.php">NPCS</a>
+                        </th>
+                        <th style="border: 1px solid black;">
+                            <a href="ItemPage.php">Items</a>
+                        </th>
+                        <th style="border: 1px solid black;">
+                            <a href="MapPage.php" style="color: white;">Maps</a>
+                        </th>
+                        <th style="border: 1px solid black;">
+                            <a href="GymPage.php">Gyms</a>
+                        </th>
+                    </tr>
+                </table>
+            </div>
+         </div>
+    </body>
+</html>
